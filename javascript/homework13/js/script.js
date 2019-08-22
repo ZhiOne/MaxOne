e0f2c272ecdb3fa088880ea28591c5c86c3f19b2
Maksym Yevtushenko <myspacetrack23@gmail.com>
2019-08-22 17:59:51 +0300

    hw 13 is done

--- javascript/homework13/js/script.js
+++ javascript/homework13/js/script.js
@@ -1,6 +1,6 @@
 //debugger
 //
-// function change() {
+// function change() {                                                     // Doesn`t work
 //
 //
 // const ChangeTheme = document.createElement('button');
@@ -32,27 +32,82 @@
 // }
 // change();
 
+//
+// let bodyItem = document.getElementsByTagName('body')[0];   // Doesn`t work
+// const ChangeTheme = document.createElement('button');
+// ChangeTheme.classList.add('change-theme-btn');
+// ChangeTheme.innerText = "Switch Theme";
+// const footer = document.getElementsByTagName('footer');
+// footer[0].before(ChangeTheme);
+// function change(){
+//
+// ChangeTheme.addEventListener('click',()=>{
+//     bodyItem.classList.toggle('bg-color');
+//     if (localStorage.getItem('bg-col') === null){
+//         localStorage.setItem('bg-col','bg-color')
+//     }else {
+//         localStorage.removeItem('bg-col')
+//     }
+// });
+// document.addEventListener('load', () => {
+//     if (localStorage.getItem('bg-col') !== null) {
+//         bodyItem.classList.add('bg-col')
+//         }
+//     })
+// }
+// change();
 
-let bodyItem = document.getElementsByTagName('body')[0];
+
+// let bodyItem = document.getElementsByTagName('body')[0]; // Это работает !!!!!!!!!!!!
+// const ChangeTheme = document.createElement('button');
+// ChangeTheme.classList.add('change-theme-btn');
+// ChangeTheme.innerText = "Switch Theme";
+// const footer = document.getElementsByTagName('footer');
+// footer[0].before(ChangeTheme);
+//
+// window.addEventListener("DOMContentLoaded",()=>{
+//
+//     if (localStorage.getItem('change-bg')!== null){
+//         bodyItem.style.backgroundColor = 'aquamarine';
+//     }
+//
+//
+//     ChangeTheme.addEventListener('click',()=>{
+//         bodyItem.style.backgroundColor = 'aquamarine';
+//         if (localStorage.getItem('change-bg') === null){
+//             localStorage.setItem('change-bg','aquamarine')
+//         }else {
+//
+//             localStorage.removeItem('change-bg');
+//             bodyItem.style.backgroundColor = 'white'
+//         }
+//     })
+// });
+
+
+let bodyItem = document.getElementsByTagName('body')[0]; // Это работает !!!!!!!!!!!!
 const ChangeTheme = document.createElement('button');
 ChangeTheme.classList.add('change-theme-btn');
 ChangeTheme.innerText = "Switch Theme";
 const footer = document.getElementsByTagName('footer');
 footer[0].before(ChangeTheme);
-function change(){
 
-ChangeTheme.addEventListener('click',()=>{
-    bodyItem.classList.toggle('bg-color');
-    if (localStorage.getItem('bg-col') === null){
-        localStorage.setItem('bg-col','bg-color')
-    }else {
-        localStorage.removeItem('bg-col')
+window.addEventListener("DOMContentLoaded",()=>{
+
+    if (localStorage.getItem('change-bg')!== null){
+        bodyItem.style.backgroundColor = 'aquamarine';
     }
-});
-    document.addEventListener('load', () => {
-        if (localStorage.getItem('bg-col') !== null) {
-        bodyItem.classList.toggle('bg-col')
+
+
+    ChangeTheme.addEventListener('click',()=>{
+        bodyItem.style.backgroundColor = 'aquamarine';
+        if (localStorage.getItem('change-bg') === null){
+            localStorage.setItem('change-bg','aquamarine')
+        }else {
+
+            localStorage.removeItem('change-bg');
+            bodyItem.style.backgroundColor = 'hotpink'
         }
     })
-}
-change();
+});
+
